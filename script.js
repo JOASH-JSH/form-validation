68005b6a9ff1e1c23d519e1ae900c79ea7141c86
JOASH BRAHMA <167842011+joash-js@users.noreply.github.com>
2024-06-06 08:23:31 +0530

    Rework complete

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const passwordInput = document.getElementById("password");
 const confirmPasswordInput = document.getElementById("confirm-password");
 const createAccountButton = document.getElementById("create-account-btn");
 
-const fnameErrorMsg = document.getElementById("error-msg-fname");
-const lnameErrorMsg = document.getElementById("error-msg-lname");
-const emailErrorMsg = document.getElementById("error-msg-email");
-const phoneNumberErrorMsg = document.getElementById("error-msg-phone-number");
-const passwordErrorMsg = document.getElementById("error-msg-passord");
-const confirmPasswordErrorMsg = document.getElementById("error-msg-confirm-password");
+const fnameErrorMsg = document.getElementById("fname-error-msg");
+const lnameErrorMsg = document.getElementById("lname-error-msg");
+const emailErrorMsg = document.getElementById("email-error-msg");
+const phoneNumberErrorMsg = document.getElementById("phone-number-error-msg");
+const passwordErrorMsg = document.getElementById("password-error-msg");
+const confirmPasswordErrorMsg = document.getElementById("confirm-password-error-msg");
 
 function nameValidation() {
     const nameFormat = /^[a-zA-z]{3,20}$/;
@@ -20,21 +20,25 @@ function nameValidation() {
     const lname = lnameInput.value;
 
     if (fname.length < 3) {
-        fnameErrorMsg.innerText = "First name must be at least 3 characters long.";
+        fnameErrorMsg.innerText = "* First name must be at least 3 characters long.";
+        fnameInput.classList.add("input-red-outline");
     } else if (!nameFormat.test(fname)) {
-        fnameErrorMsg.innerText = "First name can only contain alphabetic characters.";
+        fnameErrorMsg.innerText = "* First name can only contain alphabetic characters.";
+        fnameInput.classList.add("input-red-outline");
     } else {
         fnameErrorMsg.innerText = "";
+        fnameInput.classList.remove("input-red-outline");
     }
 
-    if (lname !== "") {
-        if (lname.length < 3) {
-            lnameErrorMsg.innerText = "Last name must be at least 3 characters long.";
-        } else if (!nameFormat.test(lname)) {
-            lnameErrorMsg.innerText = "Last name can only contain alphabetic characters.";
-        } else {
-            lnameErrorMsg.innerText = "";
-        }
+    if (lname.length < 3) {
+        lnameErrorMsg.innerText = "* Last name must be at least 3 characters long.";
+        lnameInput.classList.add("input-red-outline");
+    } else if (!nameFormat.test(lname)) {
+        lnameErrorMsg.innerText = "* Last name can only contain alphabetic characters.";
+        lnameInput.classList.add("input-red-outline");
+    } else {
+        lnameErrorMsg.innerText = "";
+        lnameInput.classList.remove("input-red-outline");
     }
 }
 
@@ -46,18 +50,23 @@ function contactValidation() {
     const phoneNumber = phoneNumberInput.value;
 
     if (!emailFormat.test(email)) {
-        emailErrorMsg.innerText = "Please enter a valid email address.";
+        emailErrorMsg.innerText = "* Please enter a valid email address.";
+        emailInput.classList.add("input-red-outline");
     } else {
         emailErrorMsg.innerText = "";
+        emailInput.classList.remove("input-red-outline");
     }
  
-    if (phoneNumber !== "") {
+    if (phoneNumber.length !== 0) {
         if (phoneNumber.length < 10) {
-            phoneNumberErrorMsg.innerText = "Phone number must be exactly 10 digits long.";
+            phoneNumberErrorMsg.innerText = "* Phone number must be exactly 10 digits long.";
+            phoneNumberInput.classList.add("input-red-outline");
         } else if (!phoneNumberFormat.test(phoneNumber)) {
-            phoneNumberErrorMsg.innerText = "Please enter a valid phone number.";
+            phoneNumberErrorMsg.innerText = "* Please enter a valid phone number.";
+            phoneNumberInput.classList.add("input-red-outline");
         } else {
             phoneNumberErrorMsg.innerText = "";
+            phoneNumberInput.classList.remove("input-red-outline");
         }
     }
 }
@@ -69,17 +78,22 @@ function passwordValidation() {
     const confirmPassword = confirmPasswordInput.value;
 
     if (password.length < 8) {
-        passwordErrorMsg.innerText = "Password must be at least 8 characters long.";
+        passwordErrorMsg.innerText = "* Password must be at least 8 characters long.";
+        passwordInput.classList.add("input-red-outline");    
     } else if (!passwordFormat.test(password)) {
-        passwordErrorMsg.innerText = "Password can contain letters, numbers, and special characters: \"!\"#$%&'()*+,-./:;<=>?@[\\]^_{}|~";
+        passwordErrorMsg.innerText = "* Password can contain letters, numbers, and special characters: \"!\"#$%&'()*+,-./:;<=>?@[\\]^_{}|~";
+        passwordInput.classList.add("input-red-outline");    
     } else {
         passwordErrorMsg.innerText = "";
+        passwordInput.classList.remove("input-red-outline");    
     }
 
     if (confirmPassword === "") {
-        confirmPasswordErrorMsg.innerText = "Please confirm your password.";
+        confirmPasswordErrorMsg.innerText = "* Please confirm your password.";
+        confirmPasswordInput.classList.add("input-red-outline");    
     } else {
         confirmPasswordErrorMsg.innerText = "";
+        confirmPasswordInput.classList.remove("input-red-outline");    
     }
 }
 
@@ -88,5 +102,3 @@ createAccountButton.addEventListener("click", () => {
     contactValidation();
     passwordValidation();
 });
-
-
